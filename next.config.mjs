ae190941bb56fdf95457c9467d6b9f8b5d8938f0
Nizamudeen N <nizamudeen@solwyz.com>
2026-04-09 22:21:54 +0530

    Upgrade Next.js for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['nexlearn.noviindusdemosites.in'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nexlearn.noviindusdemosites.in',
+      },
+    ],
   },
 };
 
